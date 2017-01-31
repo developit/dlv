4bf7b048b4861128dff22fa7605488e2f4749905
Jason Miller <jasonmiller@synacor.com>
2017-01-31 14:43:29 -0500

    165 bytes -> 128 bytes 🙌

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-export default function dlv(obj, key, def) {
-	if(!obj || !key) return def;
-	if (key.split) key = key.split('.');
-	var i=0, keyLength=key.length;
-	for (; i<keyLength && obj.hasOwnProperty(key[i]); obj = obj[key[i++]]) ;
-	return i===keyLength && typeof obj !== 'undefined' ? obj : def;
+export default function dlv(obj, key, def, p) {
+	p = 0;
+	key = key.split ? key.split('.') : key;
+	while (obj && p<key.length) obj = obj[key[p++]];
+	return obj===undefined ? def : obj;
 }
