a798a15f55f52835eb2efd7c2b08256cabfc6c65
Kevin Deisz <kevin.deisz@gmail.com>
2019-05-22 15:39:12 -0400

    Slim down by using a for/in loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default function dlv(obj, key, def, p, undef) {
 	key = key.split ? key.split('.') : key;
-	for (p = 0; p < key.length; p++) {
+	for (p in key) {
 		obj = obj ? obj[key[p]] : undef;
 	}
 	return obj === undef ? def : obj;
