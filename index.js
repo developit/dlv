588d551ba7b1aaa7b6f0bce630ba61c71804b768
Luan Muniz <luan@luanmuniz.com.br>
2019-05-23 14:13:46 -0300

    Make loop stop when it doesn't have any more obj to check
    With this change, if you have and object like { foo: "bar" } and you do dlv(obj, 'foo.bar.foobar.0.whatever')
    it will run twice instead of 5 times.
    
    Signed-off-by: Luan Muniz <luan@luanmuniz.com.br>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export default function dlv(obj, key, def, p, undef) {
 	key = key.split ? key.split('.') : key;
 	for (p = 0; p < key.length; p++) {
 		obj = obj ? obj[key[p]] : undef;
+		if(obj === undef) return def;
 	}
 	return obj === undef ? def : obj;
 }
